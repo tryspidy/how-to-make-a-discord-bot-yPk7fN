d144ca35da57c1b11b0b78f06380171ebeb57b34
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-11 13:03:05 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//discord.js is probably the easiest to learn. To install do this
+//in command prompt (Windows, Linux) or terminal (Mac)
+
+npm init
+//and then
+npm i discord.js
+
+//after that just read the discord.js.org documentation
+//and follow a couple yt guides, maybe join some servers.
